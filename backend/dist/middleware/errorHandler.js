4c1fd165d7ead1a417dbc0108e94bb227e0e8185
DilipReddyLingala <148195218+DilipReddyLingala@users.noreply.github.com>
2024-07-25 11:27:41 +0530

    git ignore updated

--- backend/dist/middleware/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    console.error('Error caught by middleware:', err);
-    res.status(500).json({ status: 500, body: { error: 'Internal server error' } });
-};
--- backend/dist/routes/hrRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { approveUser } from "../controllers/userControllers";
-const router = express.Router();
-router.post("/approveUser", approveUser);
-router.get("/__health", (req, res) => {
-    res.send("health check");
-});
-export default router;
