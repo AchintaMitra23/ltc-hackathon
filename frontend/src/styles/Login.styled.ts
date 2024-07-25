ad209a06eaf3c02e895006a553a8c6f92e674d80
Maheswara Rao Donga <dmaheswararao08@gmail.com>
2024-07-25 14:37:24 +0530

    styles

--- frontend/src/styles/Login.styled.ts
+++ frontend/src/styles/Login.styled.ts
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 
 export const StyledDiv1 = styled.div`
-    border-radius: 25px;
+  border-radius: 25px;
 `;
-
+export const LoginLogo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items:center
+`;
 export const StyledH5 = styled.h5`
-    color: brown;
-    text-transform: capitalize;
-    letter-spacing: 1px;
-    border-bottom: 2px solid;
-    padding-bottom: 10px;
+  color: brown;
+  text-transform: capitalize;
+  letter-spacing: 1px;
+  border-bottom: 2px solid;
+  padding-bottom: 10px;
 `;
 
 export const StyledSelect = styled.select`
-    padding: 10px;
-    font-weight: 700;
-    font-family: cursive;
-    border: none;
-    border-radius: 10px;
-    box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  font-weight: 700;
+  font-family: cursive;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);
 `;
 
 export const ErrorStyle = styled.span`
@@ -29,40 +33,40 @@ export const ErrorStyle = styled.span`
 `;
 
 export const LabelStyle = styled.label`
-    font-weight: 700;
-    font-size: 20px;
-    text-align: center;
-    margin: 10px 5px;
-    color: brown;
-    font-style: oblique;
-    font-family: cursive;
+  font-weight: 700;
+  font-size: 20px;
+  text-align: center;
+  margin: 10px 5px;
+  color: brown;
+  font-style: oblique;
+  font-family: cursive;
 `;
 
 export const DivStyle1 = styled.div`
-display: grid;
-    justify-items: center;
-    height: 40px;
-    margin: 35px 0;
-    border: none;
+  display: grid;
+  justify-items: center;
+  height: 40px;
+  margin: 35px 0;
+  border: none;
 `;
 
 export const ButtonStyle1 = styled.button`
-border: none;
-    background: brown;
-    color: white;
-    font-weight: 500;
-    padding: 5px 15px;
-    border-radius: 10px;
-    cursor: pointer;
+  border: none;
+  background: brown;
+  color: white;
+  font-weight: 500;
+  padding: 5px 15px;
+  border-radius: 10px;
+  cursor: pointer;
 `;
 
 export const DivStyle2 = styled.div`
-    margin: 15px 25px;
-    border: 2px solid;
-    border-radius: 10px;
-    padding: 10px;
-    background: brown;
-    color: white;
+  margin: 15px 25px;
+  border: 2px solid;
+  border-radius: 10px;
+  padding: 10px;
+  background: brown;
+  color: white;
 `;
 
 export const CheckBoxStyle1 = styled.input`
@@ -70,57 +74,57 @@ export const CheckBoxStyle1 = styled.input`
 `;
 
 export const SpanStyle1 = styled.span`
-    font-size: 15px;
-    font-weight: 700;
-    margin: 10px;
-    background: white;
-    color: black;
-    padding: 5px 10px;
-    border-radius: 10px;
-    letter-spacing: 1px;
+  font-size: 15px;
+  font-weight: 700;
+  margin: 10px;
+  background: white;
+  color: black;
+  padding: 5px 10px;
+  border-radius: 10px;
+  letter-spacing: 1px;
 `;
 
 export const UlStyle1 = styled.ul`
-    list-style-type: none;
-    margin: 20px;
-    box-shadow: 2px 2px 2px 2px orange;
-    border-radius: 10px;
-    padding: 10px 20px;
+  list-style-type: none;
+  margin: 20px;
+  box-shadow: 2px 2px 2px 2px orange;
+  border-radius: 10px;
+  padding: 10px 20px;
 `;
 
 export const LiStyle1 = styled.li`
-    margin: 10px;
+  margin: 10px;
 `;
 
 export const SpanStyle2 = styled.span`
-    font-size: 15px;
-    font-style: oblique;
-    color: brown;
-    font-weight: bold;
+  font-size: 15px;
+  font-style: oblique;
+  color: brown;
+  font-weight: bold;
 `;
 
 export const SpanStyle3 = styled.span`
-font-weight: 500;
-    letter-spacing: 1px;
+  font-weight: 500;
+  letter-spacing: 1px;
 `;
 
 export const StatusStyle = styled.span`
-    border: 0px solid;
-    padding: 5px 15px;
-    border-radius: 10px;
-    font-size: 25px;
-    text-transform: uppercase;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    background: brown;
-    color: white;
-    font-weight: bolder;
-    letter-spacing: 1px;
+  border: 0px solid;
+  padding: 5px 15px;
+  border-radius: 10px;
+  font-size: 25px;
+  text-transform: uppercase;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  background: brown;
+  color: white;
+  font-weight: bolder;
+  letter-spacing: 1px;
 `;
 
 export const StyledDiv2 = styled.div`
-text-align: center;
+  text-align: center;
 `;
 
 export const InputStyle1 = styled.input`
@@ -131,12 +135,12 @@ export const InputStyle1 = styled.input`
 `;
 
 export const SelectStyle1 = styled.select`
-    border: none;
-    font-size: 15px;
-    font-weight: 500;
-    text-align: justify;
+  border: none;
+  font-size: 15px;
+  font-weight: 500;
+  text-align: justify;
 `;
 
 export const StyleTRProfile = styled.tr`
-margin: 20px 0;
-`
+  margin: 20px 0;
+`;
